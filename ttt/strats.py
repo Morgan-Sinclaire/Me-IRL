f3debb4b64fbec52a4632b61de1676d53f8dcf1a
Morgan Sinclaire <sincm@uw.edu>
2017-03-20 15:26:21 -0700

    replaced negamax with minimax implementation, added alpha-beta pruning

--- ttt/strats.py
+++ ttt/strats.py
@@ -56,46 +56,12 @@ def one_lookahead(arr, p=-1):
             return place(i, np.where(rcd[i] == 0)[0][0])
     return random_play(arr)
 
-# def negamax(arr, p, depth=0, max_depth=9):
-#     """
-#     Given a game board and a player in {1,-1}, return 1,0,-1, depending
-#     on whether player 1 is in a winning, drawing, or losing position
-#     with the given player to move.
-#     Also return a tuple indicating a move that yields the best possible
-#     position.
-#     """
-#     b = ttt.Board()
-#     b.arr = arr
-#     if b.term():
-#         return b.win()
-#         # when p=-1, we want to indicate a win as 1
-#
-#     m = -2
-#
-#     for i in range(arr.shape[0]):
-#         for j in range(arr.shape[1]):
-#             if b.arr[i,j] == 0:
-#                 b.arr[i,j] = p
-#                 v = -negamax(arr, -p, depth+1, max_depth)
-#                 if v > m:
-#                     m = v
-#                     mi = i
-#                     mj = j
-#                 b.arr[i,j] = 0
-#
-#     if depth == 0:
-#         return m*p,(mi,mj)
-#     else:
-#         return m
-
-
 def minimax(arr, p, depth=0, max_depth=9):
     """
-    Given a game board and a player in {1,-1}, return 1,0,-1, depending
-    on whether player 1 is in a winning, drawing, or losing position
-    with the given player to move.
-    Also return a tuple indicating a move that yields the best possible
-    position.
+    Given a game board and a player in {1,-1}, return a tuple giving
+    the an optimal move under minimax.
+    For non-root calls, return 1,0,-1, depending on whether p1 is in a
+    winning, drawing, or losing position with the given player to move.
     """
     b = ttt.Board()
     b.arr = arr
@@ -133,16 +99,8 @@ def minimax(arr, p, depth=0, max_depth=9):
         else:
             return m
 
-
-
 def minimax_pruned(arr, p, depth=0, max_depth=9, alpha=-2, beta=2):
-    """
-    Given a game board and a player in {1,-1}, return 1,0,-1, depending
-    on whether player 1 is in a winning, drawing, or losing position
-    with the given player to move.
-    Also return a tuple indicating a move that yields the best possible
-    position.
-    """
+    """Same as minimax, but apply alpha-beta pruning for efficiency."""
     b = ttt.Board()
     b.arr = arr
     if b.term():
